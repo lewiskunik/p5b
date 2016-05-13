d8d49e0c0b3049bf87f746590232f04e68f2c483
lewiskunik <lkunik@wisc.edu>
2016-05-13 13:34:29 -0500

    passing a couple things

---
+++ user/fulldisk.d
@@ -0,0 +1,2 @@
+user/fulldisk.d user/fulldisk.o: user/fulldisk.c include/types.h \
+ include/stat.h user/user.h include/fcntl.h include/fs.h
---
+++ user/hello.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int
+main(int argc, char *argv[])
+{
+	int fd = open("foo.txt", O_CREATE | O_SMALLFILE);
+	printf(1, "hello %d\n", fd);
+	close(fd);
+	exit();
+
+
+}
